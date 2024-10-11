9bea3f1d8c0a8cb74686355fa6acd5c99316d902
Vergilian <sparkmart@mail.ru>
2024-10-11 21:14:23 +0300

    add module_3_1

---
+++ module_3_1.py
@@ -0,0 +1,35 @@
+calls = 0
+
+
+def string_info(string):
+    count_calls()
+    tuple_0 = tuple([len(string), string.upper(), string.lower()])
+    return tuple_0
+
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+
+def is_contains(name, listik):
+    result=0
+    count_calls()
+
+    for i in range(len(listik)):
+        if listik[i].lower() == name.lower():
+            result = True
+            break
+        else:
+            result = False
+    return result
+
+
+print(string_info('Pytnon'))
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(is_contains('one', ['One', 'Frr', 'fr']))
+print(calls)
