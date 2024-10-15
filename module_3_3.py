dd55c66389320f8d2bb7625d5710dda8c8992864
Vergilian <sparkmart@mail.ru>
2024-10-15 21:01:22 +0300

    editing module_3_3

--- module_3_3.py
+++ module_3_3.py
@@ -10,9 +10,9 @@ print_params(b=25)
 print_params(c=[1, 2, 3])
 
 values_list = [13, 'Fire', [2,3,2]]
-values_dict_ = {'a': 7,'b': 7,'c': 7}
+values_dict = {'a': 7,'b': 7,'c': 7}
 print_params(*values_list)
-print_params(**values_dict_)
+print_params(**values_dict)
 
 values_list_2 = [8,"bingo"]
 print_params(*values_list_2)
