d7b2f0afa8cb0ae2fb0405e87b79d401bb966a25
Vergilian <sparkmart@mail.ru>
2024-11-06 17:51:07 +0300

    add module_5_2

---
+++ HomeWork/module_5_2.py
@@ -0,0 +1,33 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+    def __len__(self):
+        return self.number_of_floor
+    def __str__(self):
+        return f'Название дома: {self.name}, кол-во этажей: {self.number_of_floor }'
+
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floor:
+            print(f"{new_floor} этаж - Такого этажа не существует, максимальный - {self.number_of_floor} этаж \n")
+        else:
+            count = 0
+            while new_floor!=count:
+                count += 1
+                print(count, "Этаж")
+                if new_floor == count:
+                    print('Мы приехали на выбранный Вами этаж \n' )
+
+
+
+Urban = House('Урбан', 18)
+Teacher_house = House('Учительский домик', 2)
+Urban.go_to(5)
+Teacher_house.go_to(10)
+
+
+print(Urban)
+print(Teacher_house)
+print(len(Urban))
+print(len(Teacher_house))
