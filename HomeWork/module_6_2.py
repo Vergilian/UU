2c80d9f51f18e840d3c304c6cae492948dffdd33
Vergilian <sparkmart@mail.ru>
2024-11-12 22:04:39 +0300

    change module_6_2

--- HomeWork/module_6_2.py
+++ HomeWork/module_6_2.py
@@ -1,5 +1,5 @@
 class Vehicle:
-    __COLOR_VARIANTS = ['green', 'black', 'white', 'red', ' yellow']
+    __COLOR_VARIANTS = []
 
     def __init__(self, owner, __model, __color, __engine_power):
         self.owner = str(owner)
@@ -32,7 +32,7 @@ class Vehicle:
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
-
+__COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
 # Изначальные свойства
