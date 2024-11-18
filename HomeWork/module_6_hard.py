3bfeea09d2ecb747b4646168154f713f65c12531
Vergilian <sparkmart@mail.ru>
2024-11-18 20:33:25 +0300

    add module_6_hard

---
+++ HomeWork/module_6_hard.py
@@ -0,0 +1,111 @@
+import math
+
+
+class Figure:
+    sides_count = 0
+
+    def __init__(self):
+        self.__sides = []
+        self.__color = []
+        self.filled = False
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_color(self, r, g, b):
+        return all(isinstance(group_color, int) and 0 <= group_color <= 250 for group_color in (r, g, b))
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+        # else:
+        #     print("Некорректные данные цветов")
+
+    def __is_valid_sides(self, *sides):
+        if len(sides) == self.sides_count:
+            return all(isinstance(side, int) and side > 0 for side in sides)
+        else:
+            return False
+
+    def get_sides(self):
+        return list(self.__sides)
+
+    def __len__(self):
+        return sum(self.__sides)
+
+    def set_sides(self, *new_sides):
+        if len(new_sides) == self.sides_count:
+            self.__sides = new_sides
+        # else:
+        #     print("Кол-во сторон не совпадает с текущим объектом")
+
+
+class Circle(Figure):
+    sides_count = 1
+
+    def __init__(self, color, circumference):
+        super().__init__()
+        r, g, b = color
+        self.set_color(r, g, b)
+        self.set_sides(circumference)
+        self.__radius = circumference / (2 * math.pi)
+
+    def get_square(self):
+        return math.pi * self.__radius ** 2
+
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, color, *sides):
+        super().__init__()
+        r, g, b = color
+        self.set_color(r, g, b)
+        self.set_sides(*sides)
+
+    def get_square(self):
+        a, b, c = self.get_sides()
+        p = (a + b + c) / 2
+        return math.sqrt(p * (p - a) * (p - b) * (p - c))
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color, *sides):
+        super().__init__()
+        r, g, b = color
+        self.set_color(r, g, b)
+        self.get_size(*sides)
+
+    def get_size(self, *sides):
+        if len(sides) != 1:
+            print("Для куба передаётся только одна положительная сторона")
+        else:
+            self._Figure__sides = [sides[0]] * self.sides_count
+
+    def get_volume(self):
+        return self.get_sides()[0] ** 3
+
+
+# Код для проверки:
+circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77)  # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15)  # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15)  # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
