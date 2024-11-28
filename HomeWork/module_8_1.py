0ef608461f4eb7db66efed50d732d35f88204865
Vergilian <sparkmart@mail.ru>
2024-11-28 22:15:34 +0300

    change module_8_1

--- HomeWork/module_8_1.py
+++ HomeWork/module_8_1.py
@@ -1,8 +1,9 @@
 def add_everything_up(a, b):
     try:
-        return round((a + b),3)
+        return round((a + b), 3)
     except TypeError:
-        return str(a)+str(b)
+        return str(a) + str(b)
+
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
