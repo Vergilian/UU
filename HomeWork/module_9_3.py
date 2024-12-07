e15e0da3986b9a1fded9a8a6cfdcaf5fd3096baa
Vergilian <sparkmart@mail.ru>
2024-12-07 20:42:00 +0300

    change module_9_3

--- HomeWork/module_9_3.py
+++ HomeWork/module_9_3.py
@@ -1,8 +1,8 @@
 first = ['Strings', 'Student', 'Computers']
-second = ['Строка', 'Урбан', 'Компьютер']
+second = ['Строка', 'Урбан', 'Компьютер','Процессор']
 
 first_result = (len(f) - len(s) for f, s in zip(first, second) if len(f) != len(s))
-second_result = (len(first[i]) == len(second[i]) for i in range(0, len(first)) and range(0, len(second)))
+second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
 
 print(list(first_result))
 print(list(second_result))
