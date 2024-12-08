64eb92caa3d3d68cf5fe61754d6836f083a29f3d
Vergilian <sparkmart@mail.ru>
2024-12-08 22:05:05 +0300

    add module_9_4

---
+++ HomeWork/module_9_4.py
@@ -0,0 +1,36 @@
+# Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result = map(lambda x, y: x == y, first, second)
+print(list(result))
+
+# Замыкание:
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        d_str = '\n'.join(map(str, data_set))
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(d_str)
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+#Метод __call__:
+from random import choice
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self, *words):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
