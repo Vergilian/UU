e73957a302bd36418d4156519eccf0ea7e9ad4ad
Vergilian <sparkmart@mail.ru>
2024-12-25 01:23:31 +0300

    change module_5_2

--- HomeWork/module_5_2.py
+++ HomeWork/module_5_2.py
@@ -2,23 +2,19 @@ class House:
     def __init__(self, name, number_of_floor):
         self.name = name
         self.number_of_floor = number_of_floor
+
     def __len__(self):
         return self.number_of_floor
-    def __str__(self):
-        return f'Название дома: {self.name}, кол-во этажей: {self.number_of_floor }'
 
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floor}"
 
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floor:
-            print(f"{new_floor} этаж - Такого этажа не существует, максимальный - {self.number_of_floor} этаж \n")
+            print("Такого этажа не существует")
         else:
-            count = 0
-            while new_floor!=count:
-                count += 1
-                print(count, "Этаж")
-                if new_floor == count:
-                    print('Мы приехали на выбранный Вами этаж \n' )
-
+            for i in range(1, new_floor + 1):
+                print(i)
 
 
 Urban = House('Урбан', 18)
@@ -26,8 +22,10 @@ Teacher_house = House('Учительский домик', 2)
 Urban.go_to(5)
 Teacher_house.go_to(10)
 
-
+# __str__
 print(Urban)
 print(Teacher_house)
+
+# __len__
 print(len(Urban))
 print(len(Teacher_house))
