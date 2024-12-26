71856e5137e3c61a70cadd07217de1ee303ca32b
Vergilian <sparkmart@mail.ru>
2024-12-26 20:26:51 +0300

    change module_6_1

--- HomeWork/module_6_1.py
+++ HomeWork/module_6_1.py
@@ -6,12 +6,12 @@ class Animal:
         self.name = name
 
     def eat(self, food):
-        if food.edible is False:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-        elif food.edible is True:
-            print(f"{self.name} съел {food.name}")
+        if food.edible is True:
             self.fed = True
+            print(f"{self.name} съел {food.name}")
+        else:
+            self.alive = False
+            print(f"{self.name} не стал есть {food.name}")
 
 
 class Plant:
@@ -41,8 +41,10 @@ a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
+
 print(a1.name)
 print(p1.name)
+
 print(a1.alive)
 print(a2.fed)
 a1.eat(p1)
